7f157d26d256dbf47b7227d3cb9d1a4935a0097c
alan <alanarostegui@hotmail.com>
2020-02-09 17:03:01 -0600

    environment variables

--- src/components/user/Signup.js
+++ src/components/user/Signup.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Layout from "../../core/Layout/Layout";
-
+import { config } from '../../config/index';
 
 const SignUp = () => (
   <Layout title="Signup" description="Signup to Node React E-Commerce">
-    ...
+    {config.api}
   </Layout>
 );
 
