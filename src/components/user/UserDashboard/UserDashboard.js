7b99a8b97d9bf20368b00912431de4cbd716b7ae
alan <alanarostegui@hotmail.com>
2020-02-28 21:55:05 -0600

    links on user dashboard

--- src/components/user/UserDashboard/UserDashboard.js
+++ src/components/user/UserDashboard/UserDashboard.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Layout from "../../../core/Layout/Layout";
 import { isAuthenticated } from "../../../helpers/authenticate";
+import { Link } from "react-router-dom";
 
 const UserDashboard = () => {
   const {
     user: { _id, name, email, role }
   } = isAuthenticated();
-  return (
-    <Layout
-      title="Dashboard"
-      description="User Dashboard"
-      className="container"
-    >
+
+  const userLinks = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link to="/cart" className="nav-link">
+              My Cart
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/profile/update" className="nav-link">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
         <ul className="list-group">
@@ -22,12 +40,35 @@ const UserDashboard = () => {
           </li>
         </ul>
       </div>
+    );
+  };
+
+  const purchaseHistory = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">Purchase history</h3>
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
       </div>
+    );
+  };
+
+  return (
+    <Layout
+      title="Dashboard"
+      description={`Good day ${name}`}
+      className="container"
+    >
+        <div className="row">
+            <div className="col-3">
+                { userLinks()}
+            </div>
+            <div className="col-9">
+                {userInfo()}
+                {purchaseHistory()}
+            </div>
+        </div>
     </Layout>
   );
 };
