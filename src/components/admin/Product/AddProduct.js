f094a1552daed037bdc3bd5448f3b6e3bbb406e2
alan <alanarostegui@hotmail.com>
2020-02-29 13:02:18 -0600

    create product part two (create form and functions)

--- src/components/admin/Product/AddProduct.js
+++ src/components/admin/Product/AddProduct.js
@@ -7,13 +7,130 @@ import { createProduct } from "../../../api";
 
 const AddProduct = () => {
   const { user, token } = isAuthenticated();
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    shipping: "",
+    quantity: '',
+    photo: "",
+    loading: false,
+    error: "",
+    createdProduct: "",
+    redirectToProfile: false,
+    formData: ""
+  });
+
+  const {
+    name,
+    description,
+    price,
+    categories,
+    category,
+    shipping,
+    quantity,
+    photo,
+    loading,
+    error,
+    createdProduct,
+    redirectToProfile,
+    formData
+  } = values;
+
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData()})
+  }, [])
+
+  const onChange = name => e => {
+    const value = name === "photo" ? e.target.files[0] : e.target.value;
+    formData.set(name, value)
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = e => {
+      //
+  }
+
+  const newPostForm = () => (
+    <form className="mb-3" onSubmit={onSubmit}>
+      <h4>Post Photo</h4>
+      <div className="form-group">
+        <label className="btn btn-secondary">
+          <input
+            onChange={onChange("photo")}
+            type="file"
+            name="photo"
+            accept="image/*"
+          />
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={onChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Description</label>
+        <textarea
+          onChange={onChange("description")}
+          className="form-control"
+          value={description}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Price</label>
+        <input
+          onChange={onChange("price")}
+          type="number"
+          className="form-control"
+          value={price}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Category</label>
+        <select onChange={onChange("category")} className="form-control">
+          <option value="5e5a12d179979f3f5bb5413e">Python</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Shipping</label>
+        <select onChange={onChange("shipping")} className="form-control">
+          <option value="0">No</option>
+          <option value="1">Yes</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Quantity</label>
+        <input
+          onChange={onChange("quantity")}
+          type="number"
+          className="form-control"
+          value={quantity}
+        />
+      </div>
+
+      <button className="btn btn-outline-primary">Create Product</button>
+    </form>
+  );
   return (
     <Layout
       title="Add a new product"
       description={`Good day ${user.name}, ready to add a new product?`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">...</div>
+        <div className="col-md-8 offset-md-2">{newPostForm()}</div>
       </div>
     </Layout>
   );
