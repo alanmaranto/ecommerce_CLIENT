3fd24fdc39af77f4fca5b38028de3d0818d98b1f
alan <alanarostegui@hotmail.com>
2020-03-02 20:53:59 -0600

    show products in card

--- src/view/Dashboard/Dashboard.js
+++ src/view/Dashboard/Dashboard.js
@@ -1,42 +1,53 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../core/Layout/Layout";
+
+import ProductCard from "./Product";
 import { fetchProducts } from "../../api";
 
 const Dashboard = () => {
-  const [productsBySell, setProductsBySell] = useState([])
-  const [productsByArrival, setProductsByArrival] = useState([])
-  const [error, setError] = useState(false)
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
   const loadProductsBySell = () => {
-    fetchProducts('sold').then(data => {
+    fetchProducts("sold").then(data => {
       if (data.error) {
-        setError(data.error)
+        setError(data.error);
       } else {
-        setProductsBySell(data)
+        setProductsBySell(data);
       }
-    })
-  }
+    });
+  };
 
   const loadProductsByArrival = () => {
-    fetchProducts('createdAt').then(data => {
+    fetchProducts("createdAt").then(data => {
       if (data.error) {
-        setError(data.error)
+        setError(data.error);
       } else {
-        setProductsByArrival(data)
+        setProductsByArrival(data);
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    loadProductsByArrival()
-    loadProductsBySell()
-  }, [])
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
 
   return (
-    <Layout title="Dashboard" description=" Node React E-Commerce">
-      {JSON.stringify(productsByArrival)}
-      <hr />
-      {JSON.stringify(productsBySell)}
+    <Layout title="Dashboard" description=" Node React E-Commerce" className="container">
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, index) => (
+          <ProductCard key={index} product={product}></ProductCard>
+        ))}
+      </div>
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, index) => (
+          <ProductCard key={index} product={product}></ProductCard>
+        ))}
+      </div>
     </Layout>
   );
 };
