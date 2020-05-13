747390745afa6a4ac424abd4f7daec4ebebd09e2
alan <alanarostegui@hotmail.com>
2020-05-12 19:29:06 -0500

    feature(product view): product detail

--- src/view/Dashboard/Product.js
+++ src/view/Dashboard/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
 import ProductsImages from "./ProductImages";
 
 const Card = ({ product, showViewProductButton = true }) => {
@@ -14,17 +15,40 @@ const Card = ({ product, showViewProductButton = true }) => {
       )
     );
   };
+
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-warning mt-2 mb-2">
+        Add Product to card
+      </button>
+    );
+  };
+
+  const showStock = (quantity) => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock</span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of stock</span>
+    );
+  };
+
   return (
     <div className="card">
-      <div className="card-header card-header-1">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ProductsImages item={product} url="product" />
-        <p>{product.description.substring(0, 100)}</p>
-        <p>{product.price}</p>
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="black-10">{product.price}</p>
+        <p className="black-9">
+          Category: {product.category && product.category.name}
+        </p>
+        <p className="black-8">
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
+        {showStock(product.quantity)}
+        <br />
         {showViewButton(showViewProductButton)}
-        <button className="btn btn-outline-warning mt-2 mb-2">
-          Add Product to card
-        </button>
+        {showAddToCartButton()}
       </div>
     </div>
   );
